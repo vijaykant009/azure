700c7166a89d64d1dea63119655a6a18a796a303
vijaykant009 <43712107+vijaykant009@users.noreply.github.com>
2019-06-17 22:41:43 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,10 @@
 from flask import Flask
 import os
 app = Flask(__name__)
-
+port = int(os.getenv("PORT",5000))
 @app.route("/")
 def hello():
     return "Hello Wocddrld!"
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=port)
